f1e1a67352d9bd4c1b4f0553da153efbe8717e4d
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-22 17:42:00 +0530

    Provide typed auth search params in overview

--- src/routes/_authenticated/overview.tsx
+++ src/routes/_authenticated/overview.tsx
@@ -147,7 +147,7 @@ function OverviewPage() {
         </Button>
         <div className="ml-auto flex items-center gap-1">
           <Button asChild size="sm">
-            <Link to="/auth">
+            <Link to="/auth" search={{}}>
               <LockKeyhole />
               Sign in
             </Link>
