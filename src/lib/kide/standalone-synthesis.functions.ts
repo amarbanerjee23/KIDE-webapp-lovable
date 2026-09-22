e72a57ebee9157fbd022d350fe8937309deb08dd
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-22 19:53:05 +0530

    Remove server-side KIDE computation

--- src/lib/kide/standalone-synthesis.functions.ts
+++
@@ -1,7 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-import { validateStandaloneSynthesisInput } from "@/lib/kide/standalone-synthesis-contract";
-import { verifyStandaloneSynthesis } from "@/lib/kide/standalone-synthesis.server";
-
-export const verifySynthesisOnServer = createServerFn({ method: "POST" })
-  .validator(validateStandaloneSynthesisInput)
-  .handler(async ({ data }) => verifyStandaloneSynthesis(data.files));
