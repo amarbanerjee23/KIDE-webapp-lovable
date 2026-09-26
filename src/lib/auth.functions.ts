56703a86c1c3ce20b0f5742c498d51e70e9b466f
Amar Banerjee <amarbanerjee.ska@gmail.com>
2026-09-26 15:15:32 +0530

    style(pr29): format auth server imports

--- src/lib/auth.functions.ts
+++ src/lib/auth.functions.ts
@@ -1,11 +1,6 @@
 import { createServerFn } from "@tanstack/react-start";
 import { getRequestHeaders } from "@tanstack/react-start/server";
-import {
-  authReadiness,
-  authRuntimeReadiness,
-  ensureAuthSchema,
-  getAuth,
-} from "@/lib/auth.server";
+import { authReadiness, authRuntimeReadiness, ensureAuthSchema, getAuth } from "@/lib/auth.server";
 
 export const getAuthReadiness = createServerFn({ method: "GET" }).handler(async () => {
   return authRuntimeReadiness();
